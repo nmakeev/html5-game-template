a2c475133271164da039c5e5af96f5fd46b48ec0
nmakeev <n.s.makeev@gmail.com>
2020-01-22 15:44:49 +0300

    restored somehow lost babelrc file

--- src/index.js
+++ src/index.js
@@ -15,13 +15,21 @@ app.loader.load(() => {
   sprite.x = app.screen.width * .5
   sprite.y = app.screen.height * .5
 
-  var size = Math.min(app.screen.width, app.screen.height) * .8
+  let size = Math.min(app.screen.width, app.screen.height) * .8
   sprite.width = size
   sprite.height = size
 
   app.ticker.add(delta => {
     sprite.rotation += .02 * delta
   })
+
+  let text = new PIXI.Text(`Device ratio = ${window.devicePixelRatio}`, 
+    { fontFamily: 'Arial', fontSize: 24, fill:0xffffff, align: 'center' })
+  text.anchor.set(.5)
+  app.stage.addChild(text)
+
+  text.x = app.screen.width * .5 
+  text.y = text.height * .5 + 10
 })
 
 
